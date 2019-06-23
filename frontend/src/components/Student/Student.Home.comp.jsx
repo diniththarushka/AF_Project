b295f22d1488d549a2a718642c6bcf35b78f8651
codemasterUUM <damsakaqua@gmail.com>
2019-06-23 10:15:23 +0530

    Login Components.

--- frontend/src/components/Student/Student.Home.comp.jsx
+++ frontend/src/components/Student/Student.Home.comp.jsx
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 import StudentSelectexamComp from "../Student/Student.selectexam.comp";
-import StudentEditProfileComp from "../Student/Student.editprofile.comp";
-import StudentDashboard from "../Student/Student.dashboard.comp";
-import StudentEnrolment from "../Student/Student.enrolment.comp";
+
 
 class StudentHomeComp extends Component {
     render() {
@@ -25,20 +23,15 @@ class StudentHomeComp extends Component {
                                 <li className="navbar-item">
                                     <Link to="/Student/edit-profile" className="nav-link">Edit Profile</Link>
                                 </li>
-                                <li className="navbar-item">
-                                    <Link to="/Student/enrolment" className="nav-link">Enrol</Link>
-                                </li>
                             </ul>
                         </div>
                     </nav>
                 </div>
-                <Route path="/Student/" exact component={StudentDashboard}/>
+                <Route path="/Student/" exact component={StudentSelectexamComp}/>
                 <Route path="/Student/assignments" exact component={StudentSelectexamComp}/>
                 <Route path="/Student/exams" component={StudentSelectexamComp}/>
-                <Route path="/Student/edit-profile" component={StudentEditProfileComp}/>
-                <Route path="/Student/enrolment" exact component={StudentEnrolment}/>
+                <Route path="/Student/edit-profile" component={StudentSelectexamComp}/>
             </Router>
-
         );
     }
 }
--- frontend/src/components/Student/Student.home.comp.jsx
+++ frontend/src/components/Student/Student.home.comp.jsx
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 import StudentSelectexamComp from "../Student/Student.selectexam.comp";
-import AssignmentsUpload from "./Student.AssignmentUpload.comp";
-import AddQuestion from "./Student.AddQuestion.comp";
 
 
 class StudentHomeComp extends Component {
@@ -25,24 +23,14 @@ class StudentHomeComp extends Component {
                                 <li className="navbar-item">
                                     <Link to="/Student/edit-profile" className="nav-link">Edit Profile</Link>
                                 </li>
-
-
-                                <li className="navbar-item">
-                                    <Link to="/Student/addQuestion" className="nav-link">Add Your Questions</Link>
-                                </li>
                             </ul>
                         </div>
                     </nav>
                 </div>
-
                 <Route path="/Student/" exact component={StudentSelectexamComp}/>
-                <Route path="/Student/assignments" exact component={AssignmentsUpload}/>
+                <Route path="/Student/assignments" exact component={StudentSelectexamComp}/>
                 <Route path="/Student/exams" component={StudentSelectexamComp}/>
                 <Route path="/Student/edit-profile" component={StudentSelectexamComp}/>
-
-
-                <Route path="/Student/addQuestion" exact component={AddQuestion}/>
-
             </Router>
         );
     }
