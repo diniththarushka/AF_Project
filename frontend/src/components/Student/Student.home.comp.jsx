c6812bb345b3a21541aa9c3851f1a218af5cb37c
tharinduMadhusankha96 <tharindu.madhushanka50@gmail.com>
2019-06-23 15:06:22 +0530

    Merge branch 'sub_branch01' of https://github.com/diniththarushka/AF_Project into sub_branch01
    # Conflicts:
    #	backend/node_modules/nodemailer/package.json
    #	backend/node_modules/smtp-connection/package.json
    #	backend/node_modules/underscore/package.json

--- frontend/src/components/Student/Student.home.comp.jsx
+++ frontend/src/components/Student/Student.home.comp.jsx
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Link, Route} from "react-router-dom";
 import StudentSelectexamComp from "../Student/Student.selectexam.comp";
 import StudentDashboard from "../Student/Student.dashboard.comp";
 import StudentEditProfile from "../Student/Student.editprofile.comp";
-import view from "../Instructor/Instructor.Student.Question.comp";
+
 import StudentQuestion from "./Student.AddQuestion.comp";
 import StudentAssignments from "./Student.AssignmentUpload.comp";
 
@@ -30,9 +30,7 @@ class StudentHomeComp extends Component {
                                 <li className="navbar-item">
                                     <Link to="/Student/questions" className="nav-link">Ask Your Questions</Link>
                                 </li>
-                                <li className="navbar-item">
-                                    <Link to="/Student/view" className="nav-link">View</Link>
-                                </li>
+
                             </ul>
                         </div>
                     </nav>
@@ -42,7 +40,7 @@ class StudentHomeComp extends Component {
                 <Route path="/Student/exams" component={StudentSelectexamComp}/>
                 <Route path="/Student/edit-profile" component={StudentEditProfile}/>
                 <Route path="/Student/questions" component={StudentQuestion}/>
-                <Route path="/Student/view" component={view}/>
+
             </Router>
         );
     }
